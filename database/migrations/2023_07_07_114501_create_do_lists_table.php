af72d7e5803797cf5ac4bfee7d7e6037e8b41686
wolfrik12 <107635919+wolfrik12@users.noreply.github.com>
2023-07-08 16:27:17 +0400

    Update 2023_07_07_114501_create_do_lists_table.php

--- database/migrations/2023_07_07_114501_create_do_lists_table.php
+++ database/migrations/2023_07_07_114501_create_do_lists_table.php
@@ -16,7 +16,9 @@ return new class extends Migration
         Schema::create('do_lists', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
             $table->string('time');
+            $table->string('status');
             $table->string('user_id');
         });
     }
